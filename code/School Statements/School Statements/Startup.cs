ac173a4cbac94d5404cc5833230ef618fa3d74a9
Sérgio Cardoso <cardoso.sergio@gmail.com>
2020-04-05 00:21:56 +0100

    SAML

--- code/School Statements/School Statements/Startup.cs
+++ code/School Statements/School Statements/Startup.cs
@@ -1,16 +1,27 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ITfoxtec.Identity.Saml2.MvcCore.Configuration;
+using ITfoxtec.Identity.Saml2.Util;
+using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.Schemas.Metadata;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using ITfoxtec.Identity.Saml2.MvcCore;
 
 namespace School_Statements
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public static IWebHostEnvironment AppEnvironment { get; private set; }
+
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
         {
+            AppEnvironment = env;
+
             Configuration = configuration;
         }
 
@@ -18,14 +29,40 @@ namespace School_Statements
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
 
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.Configure<Saml2Configuration>(Configuration.GetSection("Saml2"));
+            services.Configure<Saml2Configuration>(saml2Configuration =>
+            {
+                saml2Configuration.SignAuthnRequest = true;
+                saml2Configuration.SigningCertificate = CertificateUtil.Load(
+                    AppEnvironment.MapToPhysicalFilePath(Configuration["Saml2:SigningCertificateFile"]), Configuration["Saml2:SigningCertificatePassword"]);
+
+                //saml2Configuration.SignatureValidationCertificates.Add(CertificateUtil.Load(AppEnvironment.MapToPhysicalFilePath(Configuration["Saml2:SignatureValidationCertificateFile"])));
+                saml2Configuration.AllowedAudienceUris.Add(saml2Configuration.Issuer);
+
+                /*var entityDescriptor = new EntityDescriptor();
+                entityDescriptor.ReadIdPSsoDescriptorFromUrl(new Uri(Configuration["Saml2:IdPMetadata"]));
+                if (entityDescriptor.IdPSsoDescriptor != null)
+                {
+                    saml2Configuration.AllowedIssuer = entityDescriptor.EntityId;
+                    saml2Configuration.SingleSignOnDestination = entityDescriptor.IdPSsoDescriptor.SingleSignOnServices.First().Location;
+                    saml2Configuration.SingleLogoutDestination = entityDescriptor.IdPSsoDescriptor.SingleLogoutServices.First().Location;
+                    saml2Configuration.SignatureValidationCertificates.AddRange(entityDescriptor.IdPSsoDescriptor.SigningCertificates);
+                }
+                else
+                {
+                    throw new Exception("IdPSsoDescriptor not loaded from metadata.");
+                }*/
+            });
+            services.AddSaml2();
         }
+
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -42,6 +79,8 @@ namespace School_Statements
             app.UseSpaStaticFiles();
 
             app.UseRouting();
+
+            app.UseSaml2();
 
             app.UseEndpoints(endpoints =>
             {
