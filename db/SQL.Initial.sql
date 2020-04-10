18e576b7bdf21147db257f8e46745fcdc34fb067
Sérgio Cardoso <cardoso.sergio@gmail.com>
2020-04-10 17:25:10 +0100

    Update SQL.Initial.sql

--- db/SQL.Initial.sql
+++ db/SQL.Initial.sql
@@ -10,7 +10,7 @@ go
 
 create table dbo.tblConfiguration (ApplicationId uniqueidentifier rowguidcol not null, SchoolName varchar(250) not null, [Configuration] varchar(max))
 create table dbo.tblProfiles (ProfileId tinyint identity(1,1) not null, ProfileName varchar(50) not null, constraint PK_tblProfiles primary key clustered(ProfileId))
-create table dbo.tblUsers (UserId uniqueidentifier rowguidcol not null, [Name] varchar(250) not null, Email varchar(100) not null, ProfileId tinyint not null, constraint PK_tblUsers primary key clustered(UserId))
+create table dbo.tblUsers (UserId uniqueidentifier rowguidcol not null, [Name] varchar(250) not null, Email varchar(100) not null, [Password] varchar(max) not null, Salt varchar(20) not null, ProfileId tinyint not null, constraint PK_tblUsers primary key clustered(UserId))
 create table dbo.tblCurricularUnits (CurricularUnitId uniqueidentifier rowguidcol not null, CurricularUnit varchar(100) not null, constraint PK_tblCurricularUnits primary key clustered(CurricularUnitId))
 go
 
@@ -25,9 +25,9 @@ alter table dbo.tblConfiguration with check add constraint isJsonValid check (is
 alter table dbo.tblConfiguration check constraint isJsonValid
 go
 
-insert into dbo.tblProfiles (ProfileName) values ('Docente'), ('Aluno')
+insert into dbo.tblProfiles (ProfileName) values ('Docente'), ('Aluno'), ('Administrador')
 go
-insert into dbo.tblUsers ([Name], Email, ProfileId) values('Sérgio Cardoso', 'a32263@alunos.isel.pt', 1)
+insert into dbo.tblUsers ([Name], Email, ProfileId) values ('Sérgio Cardoso - D', 'cardoso.sergio@outlook.com', 1), ('Sérgio Cardoso - A', 'a32263@alunos.isel.pt', 2), ('Sérgio Cardoso - Adm', 'cardoso.sergio@gmail.com', 3)
 go
 insert into dbo.tblCurricularUnits (CurricularUnit) values
 	('Projeto e Seminário'), ('Álgebra Linear e Geometria Analítica'), ('Eletrónica'), ('Lógica e Sistemas Digitais'), ('Matemática I'), ('Programação'), ('Arquitetura de Computadores'),
@@ -41,6 +41,14 @@ if exists(select 1 from sys.procedures where name = 'spGetCurricularUnits')
 	drop procedure dbo.spGetCurricularUnits
 if exists(select 1 from sys.procedures where name = 'spSetCurricularUnits')
 	drop procedure dbo.spSetCurricularUnits
+if exists(select 1 from sys.procedures where name = 'spGetConfiguration')
+	drop procedure dbo.spGetConfiguration
+if exists(select 1 from sys.procedures where name = 'spSetConfiguration')
+	drop procedure dbo.spSetConfiguration
+if exists(select 1 from sys.procedures where name = 'spGetUser')
+	drop procedure dbo.spGetUser
+if exists(select 1 from sys.procedures where name = 'spSetUser')
+	drop procedure dbo.spSetUser
 go
 
 create procedure dbo.spGetCurricularUnits
@@ -80,4 +88,81 @@ as
 	end
 
 	select @Id 'CurricularUnitId'
+go
+
+create procedure dbo.spGetConfiguration
+(
+ @ApplicationId uniqueidentifier = null
+)
+as
+	set nocount on;
+
+	select SchoolName, Configuration
+	from tblConfiguration
+	where @ApplicationId is null or (@ApplicationId is not null and ApplicationId = @ApplicationId)
+go
+
+create procedure dbo.spSetConfiguration
+(
+ @ApplicationId uniqueidentifier,
+ @SchoolName varchar(250),
+ @Configuration varchar(max)
+)
+as
+	set nocount on;
+
+	update tblConfiguration set
+		SchoolName = @SchoolName,
+		[Configuration] = @Configuration
+	where ApplicationId = @ApplicationId
+go
+
+create procedure dbo.spGetUser
+(
+ @UserId uniqueidentifier
+)
+as
+	set nocount on;
+
+	select [Name], Email, [Password], Salt, ProfileId
+	from tblUsers
+	where UserId = @UserId
+go
+
+create procedure dbo.spSetUser
+(
+ @UserId uniqueidentifier = null,
+ @Name varchar(250),
+ @Email varchar(100),
+ @Password varchar(max),
+ @Salt varchar(20),
+ @ProfileId tinyint
+)
+as
+	set nocount on;
+
+	declare @id uniqueidentifier = @UserId
+
+	if @UserId is null
+	begin
+		declare @insert table(userId uniqueidentifier)
+
+		insert into dbo.tblUsers ([Name], Email, [Password], Salt, ProfileId)
+		output inserted.UserId into @insert
+		values (@Name, @Email, @Password, @Salt, @ProfileId)
+
+		select @id = userId from @insert
+	end
+	else
+	begin
+		update tblUsers set
+			[Name] = @Name,
+			Email = @Email,
+			[Password] = @Password,
+			Salt = @Salt,
+			ProfileId = @ProfileId
+		where UserId = @UserId
+	end
+	
+	select @id
 go
